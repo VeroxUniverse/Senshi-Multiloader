d9b64a5264cf14427c23153bfbc601b23be3df45
VeroxUniverse <stauplays@gmail.com>
2023-11-03 23:07:06 +0100

    Initialize gl3-vanillagradle

---
+++ Common/src/main/java/com/example/examplemod/client/TestEntityRenderer.java
@@ -0,0 +1,13 @@
+package com.example.examplemod.client;
+
+import com.example.examplemod.entity.TestEntity;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+
+public class TestEntityRenderer extends GeoEntityRenderer<TestEntity> {
+
+    public TestEntityRenderer(EntityRendererProvider.Context ctx) {
+        super(ctx, new TestEntityModel());
+    }
+
+}
